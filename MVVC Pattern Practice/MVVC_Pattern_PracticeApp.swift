9e1831f97c3c19939390df466c66a4adec06ffcc
Trevor Sharnick <Tythera216@gmail.com>
2024-07-11 17:22:44 -0400

    Initial Commit

---
+++ MVVC Pattern Practice/MVVC_Pattern_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVC_Pattern_PracticeApp.swift
+//  MVVC Pattern Practice
+//
+//  Created by Trevor Sharnick on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVC_Pattern_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
